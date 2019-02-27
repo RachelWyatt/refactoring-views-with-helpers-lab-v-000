9f2b747fdc68feab3cdbd2d26ec4bc0247ebec28
Rachel Wyatt <rw126@evansville.edu>
2019-02-27 13:18:07 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    Song.artist_name
   end
 
   def artist_name=(name)
+    self.artist_name = Artist.find_or_create_by(name: name)
   end
 end
